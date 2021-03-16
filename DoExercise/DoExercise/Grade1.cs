e7abf54639783b34529f4d4780e6c604b34feb30
zf <2587233135@qq.com>
2021-03-16 15:45:07 +0800

    冒泡(找到小的就交换)，选择（每次找到余下最小的index,然后交换），插入（大的后移，小的停止）的各种优势

--- DoExercise/DoExercise/Grade1.cs
+++ DoExercise/DoExercise/Grade1.cs
@@ -122,21 +122,143 @@ namespace DoExercise
 
         #region 给一组整数，按照升序排序，使用选择排序，冒泡排序，插入排序或者任何 O(n2) 的排序算法。
 
+        /// <summary>
+        /// 选择排序（循环，每次找到后面最小的）
+        /// 比冒泡好的地方在于：不是找到小的就交换，而是找到最小的才交换，其余都一样。
+        /// </summary>
+        /// <returns></returns>
         public string PX_XZ()
         {
+            int cacTimes = 0;
+            int exchangeTimes = 0;
+            int[] px_numbers = {1, 5, 0, 3, 7, 2, 8, 100, 41};
+            _outSeq.Append(ToString(px_numbers) + "=>");
+
+            int minIndex = 0;
+            for (int i = 0; i < px_numbers.Length - 1; i++)
+            {
+                minIndex = i;
+                for (int j = i + 1; j < px_numbers.Length; j++)
+                {
+                    cacTimes++;
+                    if (px_numbers[j] < px_numbers[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                if (minIndex != i)
+                {
+                    exchangeTimes++;
+                    int temp = px_numbers[minIndex];
+                    px_numbers[minIndex] = px_numbers[i];
+                    px_numbers[i] = temp;
+                }
+            }
+            //最终结果从小到大
+
+            _outSeq.Append(ToString(px_numbers));
+            _outSeq.Append($" 数组长度：{px_numbers.Length} 遍历次数：{cacTimes} 交换次数：{exchangeTimes}");
+            return _outSeq.ToString();
             return _outSeq.ToString();
         }
 
+        /// <summary>
+        /// 冒泡排序（循环，每次找到最小的那个）
+        /// 从第一个元素，对比它后面所有的元素，后面更小的就调换，遍历完毕后，最前面的就是最小的，然后接着从第二个元素...的循环
+        /// </summary>
+        /// <returns></returns>
         public string PX_MP()
         {
+            int cacTimes = 0;
+            int exchangeTimes = 0;
+            int[] px_numbers = {1, 5, 0, 3, 7, 2, 8, 100, 41};
+            _outSeq.Append(ToString(px_numbers) + "=>");
+            int temp = 0;
+            for (int i = 0; i < px_numbers.Length; i++)
+            {
+                //这个的每次遍历都让后面最小的移动到了前面
+                for (int j = i + 1; j < px_numbers.Length; j++)
+                {
+                    cacTimes++;
+                    //后一个比前一个大就交换
+                    if (px_numbers[j] < px_numbers[i])
+                    {
+                        exchangeTimes++;
+                        temp = px_numbers[j];
+                        px_numbers[j] = px_numbers[i];
+                        px_numbers[i] = temp;
+                    }
+                }
+            }
+            //最终结果从小到大
+
+            _outSeq.Append(ToString(px_numbers));
+            _outSeq.Append($" 数组长度：{px_numbers.Length} 遍历次数：{cacTimes} 交换次数：{exchangeTimes}");
             return _outSeq.ToString();
         }
 
+        /// <summary>
+        /// 插入排序（抽一个空着，后移）
+        /// 核心：
+        /// 1.抽出i位置的数（A），
+        /// 2.判断我所在位置前面一个是否比自己小，
+        /// 3.前面数大的话向后移（并不插入），比自己小的停止判断插入这个(A)，特别情况，A是最小的，跳出了循环
+        /// 循环123
+        /// 优势：本就相对有序的排序速度更快
+        /// </summary>
+        /// <returns></returns>
         public string PX_CR()
         {
+            int cacTimes = 0, exchangeTimes = 0;
+            int[] px_numbers = {1, 5, 0, 3, 7, 2, 8, 100, 41};
+            _outSeq.Append(ToString(px_numbers) + "=>");
+
+            int cur = 0;
+            for (int i = 1; i < px_numbers.Length; i++)
+            {
+                cur = px_numbers[i];
+                //i位置做空取出,与前面的那个对比，再前面对比，大的数向后移动
+                int j;
+                for (j = i - 1; j >= 0; j--)
+                {
+                    cacTimes++;
+                    if (cur < px_numbers[j])
+                    {
+                        px_numbers[j + 1] = px_numbers[j];
+                    }
+                    else
+                    {
+                        exchangeTimes++;
+                        px_numbers[j + 1] = cur;
+                        break;
+                    }
+                }
+
+                //说明到了最前面
+                if (j == -1)
+                {
+                    exchangeTimes++;
+                    px_numbers[0] = cur;
+                }
+            }
+
+            _outSeq.Append(ToString(px_numbers));
+            _outSeq.Append($" 数组长度：{px_numbers.Length} 遍历次数：{cacTimes} 交换次数：{exchangeTimes}");
             return _outSeq.ToString();
         }
 
         #endregion
+
+        private string ToString<T>(T[] objs)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < objs.Length; i++)
+            {
+                sb.Append($"  {objs[i]}");
+            }
+
+            return sb.ToString();
+        }
     }
 }
